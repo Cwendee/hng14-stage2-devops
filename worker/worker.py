c04e2b244d85050624c27b734b84a398a7ab4c5e
Prudence Anumudu <pruddyanumudu@gmail.com>
2026-04-24 12:40:49 +0100

    Fix final lint formatting issues

--- worker/worker.py
+++ worker/worker.py
@@ -4,7 +4,6 @@ import sys
 
 r = redis.Redis(host="redis", port=6379)
 
-
 try:
     print("Connected to Redis:", r.ping(), flush=True)
 except Exception as e:
@@ -21,7 +20,6 @@ def process_job(job_id):
 
 print("Worker started. Waiting for jobs...", flush=True)
 
-
 while True:
     print("Checking for job...", flush=True)
     job = r.brpop("job", timeout=5)
@@ -29,5 +27,4 @@ while True:
     if job:
         _, job_id = job
         process_job(job_id.decode())
-
-
+        
